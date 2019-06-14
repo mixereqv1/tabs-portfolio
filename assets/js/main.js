729cb61375175121bb2bb68fd7199e688fef653f
mixereqv1 <mateuszral7@gmail.com>
2019-06-14 16:16:04 +0200

    Init commit...

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const Tabs = {
+    buttons: document.querySelectorAll('.tabs__button'),
+    contents: document.querySelectorAll('.content__tabs'),
+    init: function () {
+        for (let i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].addEventListener('click', e => {
+                for (let j = 0; j < this.buttons.length; j++) {
+                    this.buttons[j].classList.remove('tabs__button__active');
+                    this.contents[j].classList.remove('content__tabs__active');
+                }
+                this.buttons[i].classList.add('tabs__button__active');
+                this.contents[i].classList.add('content__tabs__active');
+            });
+        }
+    }
+}
+Tabs.init();
