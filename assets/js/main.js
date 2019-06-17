255f9094d3a065905e8f58e4b2972ada6a37a1a6
mixereqv1 <mateuszral7@gmail.com>
2019-06-17 21:31:21 +0200

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,5 +21,5 @@ window.onload = function() {
         document.querySelectorAll('.content__tabs')[3].classList.remove('content__tabs__active');
         document.querySelectorAll('.content__tabs')[0].classList.add('content__tabs__active');
         document.querySelector('body').classList.add('loaded');
-    },1000);
+    },1500);
 }
